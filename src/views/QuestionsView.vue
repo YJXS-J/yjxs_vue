13f1a4a0e8310edc534a5cb91aca4c41add3c11e
YJXS-J <353667349@qq.com>
2022-05-14 13:26:12 +0800

    新增一些面试题

--- src/views/QuestionsView.vue
+++ src/views/QuestionsView.vue
@@ -35,8 +35,16 @@ export default {
                     answer: '变量声明：let、const;模板字符串；箭头函数等；',
                 },
                 {
-                    question: 'Vue 中双向数据绑定是如何实现的？?',
+                    question: 'Vue 中双向数据绑定是如何实现的?',
                     answer: 'vue 双向数据绑定是通过 数据劫持 结合 发布订阅模式的方式来实现的；',
+                },
+                {
+                    question: '前端性能优化?',
+                    answer: '减少HTTP请求；压缩HTML、CSS、JavaScript文件；防抖节流；懒加载；',
+                },
+                {
+                    question: '什么是防抖节流?',
+                    answer: '防抖策略（debounce）是当事件被触发后，延迟 n 秒后再执行回调，如果在这 n 秒内事件又被触发，则重新计时。以保证事件里面的代码只执行一次;节流策略（throttle），顾名思义，可以减少一段时间内事件的触发频率。',
                 },
             ],
         };
